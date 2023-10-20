ae585b0f41db4f3fac6da7efdc0da0b27e9d19b4
OverwatchUG <shadiduwaako@gmail.com>
2023-10-20 18:42:20 +0300

    fixed some errors
    some description

--- calc_area.py
+++ calc_area.py
@@ -1,24 +1,44 @@
 from abc import ABC, abstractmethod
 
 class Shape(ABC):
+  
+  # def __init__(self, name):
+  #   self.name = name
+  
   @abstractmethod
-  def shape(self, name):
-    self.name = name
-
   def calculate_area(self):
     pass
 
 class Circle(Shape):
   def calculate_area(self, radius):
-    return 3.14 * (radius * radius)
+    self.radius = radius
+    area = 3.14 * (self.radius **2)
+    print(f'The area of a circle with radius {self.radius} is {area}')
 
 class Rectangle(Shape):
-  def calculate_area(self, lenght, width):
-    return length * width
+  def calculate_area(self, length, width):
+    self.length = length
+    self.width = width
+    area =  self.length * self.width
+    print(f'The area of a triangle with length {self.length} and width {self.width} is {area}')
 
 class Triangle(Shape):
   def calculate_area(self, base, height):
-    return 0.5 * (base * height)
+    self.base = base
+    self.height = height
+    area = 0.5 * (self.base * self.height)
+    print(f'The area of a triangle with base {self.base} and height {self.height} is {area}')
+
+circle1 = Circle()
+circle1.calculate_area(int(input('Enter the radius')))
+
+rectangle1 = Rectangle()
+rectangle1.calculate_area(int(input('Enter the length')), int(input('Enter the width')))
+
+triangle1 = Triangle()
+triangle1.calculate_area(int(input('Enter the base')), int(input('Enter the height')))
+  
+
 
 
 
